9ad7d2590bab20ccda88eb357a59a8c5d79279cb
Wang <1913574520@qq.com>
2022-07-16 14:02:44 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wlq
+ * @create 2022-07-16 13:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hlo git!");
+    }
+}
---
+++ src/main/java/com/atguigu/git/jdjjd.java
@@ -0,0 +1,8 @@
+package com.atguigu.git;
+
+/**
+ * @author wlq
+ * @create 2022-07-16 13:57
+ */
+public class jdjjd {
+}
