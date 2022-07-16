70b0e75ea7ff68494cc8725e4364a6afc23cac59
Wang <1913574520@qq.com>
2022-07-16 14:17:06 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hlo git2!");
         System.out.println("hlo git3!");
         System.out.println("hlo git4!");
+        System.out.println("=hot-fix test!");
     }
 }
