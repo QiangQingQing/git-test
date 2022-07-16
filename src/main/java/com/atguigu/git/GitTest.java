9845027e630a4053a3daa52aa5242f9d92d9f699
Wang <1913574520@qq.com>
2022-07-16 14:05:58 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hlo git!");
         System.out.println("hlo git2!");
+        System.out.println("hlo git3!");
     }
 }
